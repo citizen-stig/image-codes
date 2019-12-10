35e46a5c7e50169f9f58640c6b20d760468b6be1
Nikolai Golub <nikolay.v.golub@gmail.com>
2019-12-10 16:25:20 +0100

    Append image work in progress

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,6 +1,6 @@
-use image::{DynamicImage, ImageOutputFormat, GenericImageView};
-//use image::imageops::overlay;
-use imageproc::drawing::draw_text_mut;
+use image::{DynamicImage, ImageOutputFormat, GenericImageView, ImageBuffer, Pixel};
+use image::imageops::overlay;
+use imageproc::drawing::draw_text;
 use rusttype::{FontCollection, Scale};
 
 pub struct OutputParams {
@@ -15,29 +15,33 @@ pub trait Encoder {
 
     fn params(&self) -> OutputParams;
 
-    fn append_text_to_bottom(&self, img: &mut DynamicImage) {
+    fn append_text_to_bottom(&self, img: &DynamicImage) -> DynamicImage {
         let font = Vec::from(include_bytes!("DejaVuSans.ttf") as &[u8]);
         let font = FontCollection::from_bytes(font)
             .unwrap()
             .into_font()
             .unwrap();
-        let (img_height, img_width) = (img.height(), img.width());
+        let (img_width, img_height ) = img.dimensions();
+
         let font_size: f32 = img_height as f32 * 0.2;
+        let mut img2 = ImageBuffer::new(img_width, img_height + (font_size as u32));
+        overlay(&mut img2, img, 0, 0);
+
         let scale = Scale { x: font_size, y: font_size };
 
 
         let x = 0;
-        let y = img.height() - (font_size as u32);
+        let y = img_height;
 
-        draw_text_mut(
-            img,
+        DynamicImage::ImageRgba8(draw_text(
+            &mut img2,
             image::Rgba([0u8, 0u8, 0u8, 0u8]),
             0,
             y,
             scale,
             &font,
             self.payload(),
-        );
+        ))
     }
 
     fn output(&self) -> Vec<u8> {
@@ -45,7 +49,7 @@ pub trait Encoder {
         let mut img = self.encode();
 
         if params.append_text {
-            self.append_text_to_bottom(&mut img);
+            img = self.append_text_to_bottom(&mut img);
         }
 
         let mut output_image_bytes: Vec<u8> = Vec::new();
